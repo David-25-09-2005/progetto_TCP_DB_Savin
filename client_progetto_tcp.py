00a756a9314330b344d110002d3de774faa5c5aa
Davix <128985769+David-25-09-2005@users.noreply.github.com>
2023-11-19 20:04:25 +0100

    client_progetto_tcp.py

--- client_progetto_tcp.py
+++ client_progetto_tcp.py
@@ -1,7 +1,6 @@
 import socket
 import facilities
 
-
 HOST = 'localhost'    # Il nodo remoto, qui metti il tuo indirizzo IP per provare connessione server e client dalla tua macchina alla tua macchina
 PORT = 50011     # La stessa porta usata dal server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
